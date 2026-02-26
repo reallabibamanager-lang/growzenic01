d6c665039b84c82f006972defd7af0ef78a2dcac
reallabibamanager-lang <reallabibamanager@gmail.com>
2026-02-26 19:23:37 +0530

    Update animations.ts

--- utils/animations.ts
+++ utils/animations.ts
@@ -2,3 +2,13 @@ export const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
   visible: { opacity: 1, y: 0 }
 };
+
+export const staggerContainer = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
